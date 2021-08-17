8558df6ded58e74d064e44452a3bda7aaa5b43ad
Fabian Fehrenbach <f.fehrenbach@campus.tu-berlin.de>
2021-08-17 17:40:19 +0200

    sixth try

--- src/shippingservice/semgrep/semgrep_go_script.sh
+++ src/shippingservice/semgrep/semgrep_go_script.sh
@@ -5,5 +5,5 @@ categories="setup bestpractice coverage recommendation"
 # Run semgrep rules for each category.
 # if you want to send the results to ukulele, add ["-o", "http(s)://<IP>:<PORT>/<CATEGORY>","--json"] 
 for category in $categories; do
-    semgrep --config ./src/shippingservice/semgrep/$category-go.yaml ./src/shippingservice/. -o http://[$1]:$2/$category --json
+    semgrep --config ./src/shippingservice/semgrep/$category-go.yaml ./src/shippingservice/. -o http://$1:$2/$category --json
 done
